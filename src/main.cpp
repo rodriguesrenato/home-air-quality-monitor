3515601c605821e41bfeee3615a2aa6f0400a2ce
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2025-07-20 20:21:26 +0100

    feat: Base example to read sensors and show on the TFT display

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,52 @@
 #include <Arduino.h>
-// #include <bsec2.h>
+
 #include <TFTDisplay.h>
+#include <SensorsModule.h>
 
 TFTDisplay display;
-int hour=0;
-int minute=0;
-/* Entry point for the example */
+SensorsModule sensors;
+
+int hour = 3;
+int minute = 0;
+
 void setup(void)
 {
-  display.begin();
   Serial.begin(115200);
+
+  Wire.begin();
+  sensors.setup();
+
+  display.begin();
 }
 
-/* Function that is looped forever */
 void loop(void)
 {
-  // TODO: get time from NTP server, 
+  // TODO: get time from NTP server,
   // then convert into a long (HH*60*60 + MM*60) and save as local_hh_mm
   // save the SS*1000 into time_offset_ms
   // then store it in a varible local_time_ms
   // On every loop, (millis() + time_offset_ms) %(60*1000) != local_hh_mm
   // if yes,update display with local_hh_mm
+
+  // Temporary simulation of time to show on display
+  if (minute + 1 > 59)
+  {
+    hour = (hour + 1) % 23;
+  }
+  minute = (minute + 1) % 60;
   
-  if(minute + 1 > 59){
-    hour = (hour +1)%23;
-  } 
+  display.updateTime(hour, minute);
+  
+  // Update all sensors fields on the display if the sensor reading was successfull
+  if (sensors.update()){
+    display.updateIAQ(sensors.getIAQ());
+    display.updateTemperature(sensors.getTemperature());
+    display.updateHumidity(sensors.getHumidity());
+    display.updateGas(sensors.getGas());
+    display.updateCO2(sensors.getCO2());
+    display.updateVOC(sensors.getVOC());
+  }
 
-  minute = (minute +1)%60;
-
-  display.updateTime(hour,minute);
-  display.updateIAQ((float)minute);
-  display.updateTemperature((float)(minute-30));
-  display.updateHumidity((float)minute);
-  display.updateGas((float)minute);
-  display.updateCO2((float)minute);
-  display.updateVOC((float)minute);
-  delay(100);
+  // Simple ~1s to simulate sample/update frequency
+  delay(1000);
 }
